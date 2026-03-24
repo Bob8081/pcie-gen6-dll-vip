dc84b4f516c5493d0c8f47b2090edc823b6a7ac6
Mohamed Atallah <ombob456@gmail.com>
2026-03-25 00:16:33 +0200

    LPIF Interface & Mock-PHY with tie-offs

---
+++ tb/pcie_lpif_if.sv
@@ -0,0 +1,43 @@
+interface pcie_lpif_if #(
+    parameter NBYTES = 64
+)(
+    input logic lclk,
+    input logic rst_n
+);
+
+    // Tx Path (DLL -> PHY) : "lp_" signals
+    logic [(NBYTES*8)-1:0] lp_data;         // Data Payload
+    logic [NBYTES-1:0]     lp_valid;        // 1 valid bit per byte
+    logic                  lp_irdy;         // Data Link layer ready to send
+    logic [3:0]            lp_state_req;    // Power/Link state request
+
+    // Framing Flags (1 bit per byte for variable-length packets)
+    logic [NBYTES-1:0]     lp_tlpstart;
+    logic [NBYTES-1:0]     lp_tlpend;
+    logic [NBYTES-1:0]     lp_dlpstart;
+    logic [NBYTES-1:0]     lp_dlpend;
+
+
+    // Rx Path (PHY -> DLL) : "pl_" signals
+    logic [(NBYTES*8)-1:0] pl_data;        
+    logic [NBYTES-1:0]     pl_valid;       
+    logic                  pl_trdy;        // PHY ready to accept data
+    logic [3:0]            pl_state_sts;   // Power/Link state status
+    logic                  pl_lnk_up;      // Link Up status
+
+    // Received Framing Flags
+    logic [NBYTES-1:0]     pl_tlpstart;
+    logic [NBYTES-1:0]     pl_tlpend;
+    logic [NBYTES-1:0]     pl_dlpstart;
+    logic [NBYTES-1:0]     pl_dlpend;
+    logic [NBYTES-1:0]     pl_tlpedb;      // TLP End Bad (Error injection)
+
+
+    // Static Configuration & Tie-offs (Driven by Mock PHY)
+    logic [2:0]            pl_lnk_cfg;     // Link width
+    logic [2:0]            pl_speedmode;   // Link speed
+    logic                  pl_inband_pres; // In-band presence
+    logic                  pl_error;       // Recoverable framing error
+    logic                  pl_cerror;      // Correctable error
+
+endinterface
