c0173ce3360b22e49dc751c2bb73a3a3bf07e6b8
ziadagha <ziadagha33@gmail.com>
2026-04-19 06:30:20 +0200

    fixed driver errors

--- vip/agents/pcie_dll_tx_drv.sv
+++ vip/agents/pcie_dll_tx_drv.sv
@@ -1,13 +1,13 @@
 
-class pcie_dll_tx_drv extends uvm_driver #(pcie_dll_dllp_txn);
+class pcie_dll_tx_drv extends uvm_driver #(pcie_dll_base_seq_item);
 
     //Declaration
     pcie_dll_role_e role;
     //pcie_dll_dllp_txn req;
     virtual pcie_lpif_if vif;
-    pcie_dll_base req;
-    pcie_dll_dllp_txn dllp_txn;
-    pcie_dll_tllp_txn tllp_txn;
+    pcie_dll_base_seq_item req;
+    pcie_dll_dllp_seq_item dllp_txn;
+    pcie_dll_tlp_seq_item tlp_txn;
     bit txn_type;
 
   `uvm_component_utils(pcie_dll_tx_drv)
@@ -20,52 +20,57 @@ class pcie_dll_tx_drv extends uvm_driver #(pcie_dll_dllp_txn);
 
     //build
     function void build_phase(uvm_phase phase);
-        super.build_phase(phase);    
+        super.build_phase(phase);   
+        if(!uvm_config_db#(virtual pcie_lpif_if)::get(this, "", "vif", vif)) begin
+            `uvm_fatal("NOVIF", "Virtual interface not found in configuration database")
+        end 
     endfunction
 
     //connection
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
+
     endfunction
 
-    // Assuming 'req' is your pcie_dll_base handle
 
-    if ($cast(dllp_txn, req)) begin
-    // Successfully cast to dllp_txn
-        `uvm_info("CAST", "Successfully cast to DLLP", UVM_LOW);
-        txn_type =1;
-    // Continue your logic for DLLP here
-        end 
-    else if ($cast(tllp_txn, req)) begin
-        // Successfully cast to tllp_txn
-        `uvm_info("CAST", "Successfully cast to TLLP", UVM_LOW)
-        // Continue your logic for TLLP here
-        txn_type =0;
-        end 
-    else begin
-    // Both casts failed
-    `uvm_fatal("CAST_FAIL", "Fatal Error: req is neither DLLP nor TLLP!")
-        end
 
 
   task run_phase(uvm_phase phase);
 
         forever begin
-            
             seq_item_port.get_next_item(req);
 
-            if (txn_type==1) begin
-                vif.cb_drv.lp_irdy    <= 1'b1;            // Data Link layer ready to send
-                vif.cb_drv.lp_data     <= req .lp_data;  // Data Payload
-                vif.cb_drv.lp_valid     <= 'b111_111;      // 1 valid bit per byte
+            if ($cast(dllp_txn, req)) begin
+            // Successfully cast to dllp_txn
+                `uvm_info("CAST", "Successfully cast to DLLP", UVM_LOW)
+                txn_type =1;
+            end 
+            else if ($cast(tlp_txn, req)) begin
+                // Successfully cast to tlp_txn
+                `uvm_info("CAST", "Successfully cast to TLP", UVM_LOW)
+                txn_type =0;
 
-                vif.cb_drv.lp_dlpstart     <= 'b0;  // the start byte of the data link layer 
-                vif.cb_drv.lp_dlpend       <= 'b1001; //'b1001   // the start byte of the data link
+                end 
+            else begin
+            // Both casts failed
+            `uvm_fatal("CAST_FAIL", "Fatal Error: req is neither DLLP nor TLP!")
+            end
+
+            if (txn_type==1) begin
+                @(posedge vif.lclk);
+                vif.lp_irdy    <= 1'b1;            // Data Link layer ready to send
+                vif.lp_data     <= dllp_txn.dllp;  // Data Payload
+                vif.lp_valid     <= 'b111_111;      // 1 valid bit per byte
+
+                vif.lp_dlpstart     <= 'b0;  // the start byte of the data link layer 
+                vif.lp_dlpend       <= 'b1001; //'b1001   // the start byte of the data link
                 
             end
-            else begin
-                //
-            end
+
+            // else begin
+            //    
+            // end
+
          // Tx Path (DLL -> PHY) : "lp_" signals
             // vif.cb_drv.lp_irdy     <= 1'b1;            // Data Link layer ready to send
             // vif.cb_drv.lp_data     <= req.lp_data;  // Data Payload
@@ -83,7 +88,7 @@ class pcie_dll_tx_drv extends uvm_driver #(pcie_dll_dllp_txn);
             seq_item_port.item_done();
         end
     endtask
-endclass
+
 
 
 endclass : pcie_dll_tx_drv
