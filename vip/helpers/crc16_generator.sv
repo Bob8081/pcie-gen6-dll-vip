11565cf021cfa66325287956ef709f82cea0e78d
Mohamed Atallah <ombob456@gmail.com>
2026-04-26 13:45:22 +0300

    Upadeted CRC helper function logic

---
+++ vip/helpers/crc16_generator.sv
@@ -0,0 +1,18 @@
+class crc16_generator ;
+
+    static function automatic bit [15:0] calculate_dllp_crc (bit [31:0] data);
+    bit [15:0] crc_reg = 16'hFFFF;  
+    bit [15:0] poly = 16'h8005;     
+
+    for (int i = 31; i >= 0; i--) begin
+      if (crc_reg[15] ^ data[i]) begin
+        crc_reg = (crc_reg << 1) ^ poly;
+      end else begin
+        crc_reg = crc_reg << 1;
+      end
+    end
+    
+    return ~crc_reg; 
+  endfunction
+
+endclass : crc16_generator
